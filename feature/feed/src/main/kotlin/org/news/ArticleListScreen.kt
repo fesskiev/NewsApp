d7ce46daa7c2e248d40030c80df52b7da0b2ad25
m.konchakovskiy <m.konchakovskiy@gmail.com>
2025-10-04 10:37:03 +0300

    add query, refresh logic

--- feature/feed/src/main/kotlin/org/news/ArticleListScreen.kt
+++ feature/feed/src/main/kotlin/org/news/ArticleListScreen.kt
@@ -1,25 +1,40 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package org.news
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
@@ -32,41 +47,81 @@ fun ArticleListScreen(
 ) {
     val state by viewModel.uiState.collectAsState()
 
-    LaunchedEffect(Unit) {
-        viewModel.onAction(ArticleListAction.FetchArticles)
-    }
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-
-        val error = state.error
-        if (error != null) {
-            Text(
-                text = error,
-                color = MaterialTheme.colorScheme.error
-            )
-        }
-
-        if (state.isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(100.dp)
-            )
-        }
-
-        LazyColumn {
-            items(state.articles) { article ->
-                ListItem(
-                    headlineContent = { Text(article.title, fontWeight = FontWeight.Bold) },
-                    supportingContent = { Text(article.description) },
-                    leadingContent = { article.author },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { onArticleClick(article) }
-                        .padding(vertical = 8.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("News App") },
+                actions = {
+                    Row(
+                        modifier = Modifier
+                            .wrapContentWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        TextField(
+                            value = state.query,
+                            onValueChange = { viewModel.onAction(ArticleListAction.UpdateQuery(it)) },
+                            label = { Text("Search") },
+                            modifier = Modifier.weight(1f),
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent
+                            )
+                        )
+                        IconButton(
+                            onClick = { viewModel.onAction(ArticleListAction.RefreshArticles) }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Refresh,
+                                contentDescription = "Refresh",
+                                modifier = Modifier.size(48.dp)
+                            )
+                        }
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFFCFCFC),
+                    titleContentColor = MaterialTheme.colorScheme.primary
                 )
-                HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
+            )
+        }
+    ){ paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+
+            val error = state.error
+            if (error != null) {
+                Text(
+                    text = error,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+
+            if (state.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.articles) { article ->
+                    ListItem(
+                        headlineContent = { Text(article.title, fontWeight = FontWeight.Bold) },
+                        supportingContent = { Text(article.description) },
+                        leadingContent = { Text(article.author) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { onArticleClick(article) }
+                            .padding(vertical = 8.dp)
+                    )
+                    HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
+                }
             }
         }
     }
--- feature/feed/src/main/kotlin/org/news/ArticleListViewMode.kt
+++ feature/feed/src/main/kotlin/org/news/ArticleListViewMode.kt
@@ -1,6 +1,8 @@
 package org.news
 
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.news.common.mvi.MviViewModel
@@ -10,15 +12,19 @@ import org.news.model.Article
 
 data class ArticleListState(
     val articles: List<Article> = emptyList(),
+    val query: String = "",
     val isLoading: Boolean = false,
     val error: String? = null
 )
 
-sealed class ArticleListAction {
-    data object FetchArticles : ArticleListAction()
+sealed interface ArticleListAction {
+    data object RefreshArticles : ArticleListAction
+    data class UpdateQuery(val query: String) : ArticleListAction
 }
 
-sealed class ArticleListEvent
+sealed interface ArticleListEvent {
+    data object EmptyQuery: ArticleListEvent
+}
 
 class ArticleListViewModel(
     private val repository: NewsRepository
@@ -26,20 +32,40 @@ class ArticleListViewModel(
     initialState = ArticleListState()
 ) {
 
+    private var queryJob: Job? = null
+
     override fun onAction(action: ArticleListAction) {
         when (action) {
-            is ArticleListAction.FetchArticles -> fetchArticles()
+            is ArticleListAction.RefreshArticles -> refreshArticles()
+            is ArticleListAction.UpdateQuery -> updateQuery(action.query)
         }
     }
 
-    private fun fetchArticles() {
+    private fun refreshArticles() {
+        val query = uiState.value.query
+        if (query.isEmpty()) {
+            emitUiEvent(ArticleListEvent.EmptyQuery)
+            return
+        }
+        fetchArticles(query)
+    }
+
+    private fun updateQuery(query: String) {
+        uiState.update { it.copy(query = query) }
+
+        queryJob?.cancel()
+        queryJob = viewModelScope.launch {
+            delay(500)
+            fetchArticles(query)
+        }
+    }
+
+    private fun fetchArticles(query: String) {
         viewModelScope.launch {
             executeWithTryCatch {
-                uiState.update {
-                    it.copy(isLoading = true, error = null)
-                }
+                uiState.update { it.copy(isLoading = true, error = null) }
                 val articles = repository.searchNews(
-                    query = "android",
+                    query = query,
                     from = "2025-09-29",
                     to = "2025-09-30"
                 )

0fc18204cec08b4cdbc2d6f0c932d7cf25ca000e
m.konchakovskiy <m.konchakovskiy@gmail.com>
2026-01-14 19:22:45 +0200

    fix di

--- core/security/src/main/kotlin/org/news/security/di/SecurityModule.kt
+++ core/security/src/main/kotlin/org/news/security/di/SecurityModule.kt
@@ -1,5 +1,6 @@
 package org.news.security.di
 
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 import org.news.security.keys.BiometricAuthenticationKeyManager
 import org.news.security.keys.BiometricAuthenticationKeyManagerImpl
@@ -12,8 +13,14 @@ import java.security.KeyStore
 internal const val ANDROID_KEYSTORE_PROVIDER = "AndroidKeyStore"
 
 val securityModule = module {
+    single { Dispatchers.IO }
     single { KeyStore.getInstance(ANDROID_KEYSTORE_PROVIDER).apply { load(null) } }
     single<BiometricAuthenticationKeyManager> { BiometricAuthenticationKeyManagerImpl(keyStore = get()) }
-    single<TokenEncryptionKeyManager> { TokenEncryptionKeyManagerImpl(keyStore = get()) }
+    single<TokenEncryptionKeyManager> {
+        TokenEncryptionKeyManagerImpl(
+            dispatcher = get(),
+            keyStore = get()
+        )
+    }
     single<BiometricManager> { BiometricManagerImpl(context = get()) }
 }
